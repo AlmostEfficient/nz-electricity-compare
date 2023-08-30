487cb0591b4f22b58f19167d721f1625276f8aaf
Abdullah Raza <42661870+AlmostEfficient@users.noreply.github.com>
2023-08-30 16:23:15 +1200

    init

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -3,7 +3,12 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head >
+        <link rel="icon" href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>⚡</text></svg>"></link>
+        <meta name="description" content="Compare electricity plans in New Zealand"></meta>
+        <meta property="og:title" content="Power Compare NZ" key="title" />
+        <meta property="og:description" content="Get cheaper electricity in New Zealand" key="description" />
+      </Head>
       <body>
         <Main />
         <NextScript />
